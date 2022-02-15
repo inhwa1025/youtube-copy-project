9a857cbc4f49e0f7f4e10378f18897aa9df12ff5
inhwa1025 <inhwa9901@gmail.com>
2022-02-15 15:36:02 +0900

    FIX typo

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,10 +4,7 @@ import styles from './card.module.css';
 class Card extends Component {
   render() {
     return (
-      <div 
-        className={styles.card}
-        onClick={this.props.videoClick}
-      >
+      <div className={styles.card} onClick={this.props.videoClick}>
         <img
           className={styles.thumbnails}
           src={this.props.thumbnails}
@@ -16,7 +13,6 @@ class Card extends Component {
           <li className={styles.videoTitle}>{this.props.videoTitle}</li>
           <li className={styles.channelTitle}>{this.props.channelTitle}</li>
         </span>
-        
       </div>
     );
   }
