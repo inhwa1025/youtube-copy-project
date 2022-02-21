3393a94df08fc7d064f82f88e6885ae19d58bc45
inhwa1025 <inhwa9901@gmail.com>
2022-02-21 14:43:17 +0900

    REFACTOR

--- src/app.jsx
+++ src/app.jsx
@@ -5,34 +5,18 @@ import VideoList from './components/video_list/video_list';
 import PlayVideo from './play_video/play_video';
 import axios from "axios";
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBBqYH4Y2P6B9SriMUcyj6viCRiXzllfS8`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id:item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)//
+    .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBBqYH4Y2P6B9SriMUcyj6viCRiXzllfS8", 
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()//
+    .then(videos => setVideos(videos));
   }, []);
 
   return (
