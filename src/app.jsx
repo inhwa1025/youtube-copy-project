ac148fbd7b6ba1e3bb7587c761deebf22669810b
inhwa1025 <inhwa9901@gmail.com>
2022-02-21 15:55:26 +0900

    FEAT: select Video & play

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import style from './app.module.css';
+import styles from './app.module.css';
 import Header from './components/header/header';
 import VideoList from './components/video_list/video_list';
 import PlayVideo from './play_video/play_video';
 import axios from "axios";
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null)
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const search = query => {
     youtube
     .search(query)//
@@ -20,11 +27,23 @@ function App({youtube}) {
   }, []);
 
   return (
-    <div className={style.app}>
-      <Header onSearch={search}/>
-      <VideoList 
-        videoList={videos}
-      />
+    <div className={styles.app}>
+      <Header onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList 
+            videoList={videos}
+            onVideoClick={selectVideo}
+            display={selectVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
+      
       {/* <PlayVideo 
         Video={this.state.selectVideo}
       /> */}
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import VideoItem from '../video_item/video_item';
 import styles from './video_list.module.css';
 
-const VideoList = (props) => (
+const VideoList = ({ videoList, onVideoClick, display }) => (
   <div className={styles.videos}>
-    {props.videoList.map(video =>(
+    {videoList.map(video =>(
       <VideoItem 
         key={video.id}
-        videoTitle={video.snippet.title}
-        channelTitle={video.snippet.channelTitle}
-        thumbnails={video.snippet.thumbnails.medium.url}
-        // videoClick={this.videoClick}
+        video={video}
+        onVideoClick={onVideoClick}
+        display={display}
       />
     ))}
   </div>
